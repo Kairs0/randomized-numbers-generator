3fe41017dbb51f2107dce9ec434e2a309a4a4f3f
Kairs0 <arnaud.convers@gmail.com>
2018-03-07 00:45:12 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Randomized Numbers Generator
+
+Testings algorithms for generating pseudo randomized numbers
+
+## Linear Congruential
+
+https://en.wikipedia.org/wiki/Linear_congruential_generator
---
+++ performances.py
@@ -0,0 +1,12 @@
+from linear_congruential import LCG
+
+def test_performances():
+	a = LCG()
+	a.set_seed(23)
+	array_result = [0 for i in range(100)]
+	for i in range(10000000):
+		generated = a.generate() % 100
+		array_result[generated - 1] += 1
+
+	result_per_cent = [str((number / 10000000) * 100) + "%" for number in array_result]
+	print(result_per_cent)

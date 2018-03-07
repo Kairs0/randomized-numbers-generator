d45f5a497da35f270a520bda5ece6b8f488543a8
Kairs0 <arnaud.convers@gmail.com>
2018-03-07 21:54:32 +0100

    update doc and LNG

--- linear_congruential.py
+++ linear_congruential.py
@@ -1,19 +1,19 @@
 class LCG(object):
 
-	def __init__(self):
-		self.seed = 0
-		self.modulus = 2**32
-		self.multiplier = 1664525
-		self.increment = 1013904223
-		self.interval_max = 100
+    def __init__(self):
+        self.seed = 0
+        self.modulus = 2 ** 32
+        self.multiplier = 1664525
+        self.increment = 1013904223
+        self.interval_max = 100
 
-	def set_interval_max(self, interval):
-		self.interval_max = interval
+    def set_interval_max(self, interval):
+        self.interval_max = interval
 
-	def set_seed(self, n):
-		self.seed = n
+    def set_seed(self, n):
+        self.seed = n
 
-	def generate(self):
-		result = (self.multiplier * self.seed + self.increment) % self.modulus
-		self.seed = result
-		return result % self.interval_max
+    def generate(self):
+        result = (self.multiplier * self.seed + self.increment) % self.modulus
+        self.seed = result
+        return result % self.interval_max
--- performances.py
+++ performances.py
@@ -1,12 +1,13 @@
 from linear_congruential import LCG
 
-def test_performances():
-	a = LCG()
-	a.set_seed(23)
-	array_result = [0 for i in range(100)]
-	for i in range(10000000):
-		generated = a.generate() % 100
-		array_result[generated - 1] += 1
 
-	result_per_cent = [str((number / 10000000) * 100) + "%" for number in array_result]
-	print(result_per_cent)
+def test_performances():
+    a = LCG()
+    a.set_seed(23)
+    array_result = [0 for i in range(100)]
+    for i in range(10000000):
+        generated = a.generate() % 100
+        array_result[generated - 1] += 1
+
+    result_per_cent = [str((number / 10000000) * 100) + "%" for number in array_result]
+    print(result_per_cent)
